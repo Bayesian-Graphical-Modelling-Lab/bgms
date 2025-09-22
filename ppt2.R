029e7cfe663c59642839ed82e45b4da8268bbdf9
Don van den Bergh <donvdbergh@hotmail.com>
2025-09-22 15:09:55 +0200

    cleanup

--- ppt2.R
+++
@@ -1,4 +0,0 @@
-Rcpp::sourceCpp("src/progress_manager.cpp")
-runMCMC_parallel(3, 500)
-runMCMC_parallel(3, 500, useUnicode = FALSE)
-runMCMC_parallel(3, 500, useUnicode = FALSE, display_progress = TRUE)
